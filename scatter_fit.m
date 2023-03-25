9f455cb17fc39b6ae39d6805bf4c632ed03ff6f4
marcosp-araujo <88653954+marcosp-araujo@users.noreply.github.com>
2023-03-25 21:00:43 +0100

    Add files via upload

--- scatter_fit.m
+++ scatter_fit.m
@@ -1,9 +1,11 @@
 %% SCATTER PLOT AND REGRESSION LINE FITTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% This function creates a scatter plot and computes the linear regression 
-%%% between the two scattered data. It provides RMSE and R^2 as goodness of
-%%% fit indicators
+%%% between the two scattered data. It provides root-mean-squared error
+%%% (RMSE) and coefficient of determination (R^2) as godness of fit
+%%% indicators
+
 %%% AUTHOR: Marcos P. Araújo da Silva (https://github.com/marcosp-araujo)
-%%% version 1.0, 22 March 2022
+%%% version 1.1, 25 March 2022
 
 %% VARIABLES DESCRIPTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% INPUTS (MANDATORY)
@@ -39,31 +41,18 @@ function [gof,p_sca,p_rl,p_ref] = scatter_fit(x,y,options)
        y
        options.color = [];   
        options.sca = 1; 
-       options.reference = 1; 
+       options.reference = 0; 
        options.regression  = 1;  
        options.limits = 'auto'    
    end
+   
 %%% SELECTING non-NaN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
     data(:,1) = x; %forcing to be column
     data(:,2) = y; %forcing to be column
     indx  = all(~isnan(data),2); %Index of non-NaN
     x = data(indx,1); % x must be a column vector
     y = data(indx,2); % y must be a column vector
-    
-%%% AXIS CONFIGURATION  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    if strcmp(options.limits,'auto')
-        min_all = min([x,y],[],'all');
-        max_all = max([x,y],[],'all');
-    else        
-        min_all = min(options.limits);
-        max_all = max(options.limits);
-    end
-        xlim([min_all max_all])  %%% Defining axis limits
-        ylim([min_all max_all])  %%% Defining axis limits
-    box on
-    grid on
-    set(gca,'fontsize',14,'TickDir','out')
-      
+   
 %%% SCATTER PLOT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
     hold on
     if options.sca == 1 %show scatter plot
@@ -77,18 +66,23 @@ function [gof,p_sca,p_rl,p_ref] = scatter_fit(x,y,options)
     else
         p_sca = [];
     end
+%%% GETTING LIMITS FOR THE PLOT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    x_limits = set_limits(x,options);
+    y_limits = set_limits(y,options);
+    
+
     
 %%% REGRESSION LINE FITTING  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     [lin_model,gof] = fit(x,y,'poly1'); %linear fitting 
     %gof has the regression line goodness of fitting
     N = length(x); %number of samples
-    x_fit = linspace(min_all,max_all,N)'; % x-data for regression line
+    x_fit = linspace(x_limits(1),x_limits(2),N);
     y_fit = lin_model(x_fit);     % y-data for regression line
     gof.slope = lin_model.p1;     % Slope
     gof.intercept = lin_model.p2; % Intercept
     gof.r = sqrt(gof.rsquare);    % Coefficient of correlation
     
-    %%% RMSE comparing 'x' to 'y' data directly
+%%% RMSE comparing 'x' to 'y' data directly
     RMSE = sqrt(mean((y - x).^2));
     gof.RMSE = RMSE;
     
@@ -104,9 +98,9 @@ function [gof,p_sca,p_rl,p_ref] = scatter_fit(x,y,options)
     %%% LEGEND SHOWING GOODNESS OF FITTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         m_str = num2str(gof.slope,'%0.3fx');    %Slope
         n_str = num2str(gof.intercept,'%+0.3f');%Intercept
-        rsquare_str = num2str(gof.rsquare,'%0.3f');  %Coefficient of determination
+        rsquare_str = num2str(gof.rsquare,'%0.3f'); %Coefficient of determination
         RMSE_str = num2str(gof.RMSE,'%0.3f');   %Root-mean-square error
-        %RMSE_str = num2str(gof.rmse,'%0.3f');  Gof RMSE 
+        %RMSE_str = num2str(gof.rmse,'%0.3f');  %Gof RMSE 
         N = num2str(sum(~isnan(x))); %Number of samples
         fit_str = strcat(m_str,{' '},n_str,...
                           ', RMSE = ', RMSE_str,...
@@ -114,7 +108,28 @@ function [gof,p_sca,p_rl,p_ref] = scatter_fit(x,y,options)
                           ' (N = ', N,')');
 
         le = legend(p_rl,fit_str);
-        set(le,'FontSize',10,'Location','northwest','Box','off');
+        set(le,'FontSize',11,'Location','northwest','Box','off');
     end
+
+    %%% PLOT CONFIGURATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+    set(gca,'fontsize',13,'TickDir','out')
+    xlim(x_limits) %%% Defining x-axis limits
+    ylim(y_limits) %%% Defining y-axis limits
+    
+    box on
+    grid on
     hold off
+     
+end
+
+%% Defining axes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function data_limits = set_limits(data,options)
+
+    if strcmp(options.limits,'auto') %automatic limits
+        data_limits(1) = min(data) - abs(min(data)*.02); 
+        data_limits(2) = max(data) + abs(max(data)*.02);  
+    else %else, if 'limits' are inputted:
+        data_limits = options.limits;
+    end
+    
 end
